ab6c15562def3c2ca879c76367d45fd616d99639
meyyh <94275329+meyyh@users.noreply.github.com>
2023-11-29 14:26:28 -0600

    Create btr-file-search.ps1

---
+++ scripts/Windows/btr-file-search.ps1
@@ -0,0 +1,1 @@
+gci -r -fi *.jpg,*.png,*.aac,*.ac3,*.avi,*.aiff,*.bat,*.bmp,*.exe,*.flac,*.gif,*.jpeg,*.mov,*.m3u,*.m4p,*.mp2,*.mp3,*.mp4,*.mpeg4,*.midi,*.msi,*.ogg,*.png,*.txt,*.sh,*.wav,*.wma,*.vqf,*.pcap,*.zip,*.pdf,*.json 'C:\Users\*'
