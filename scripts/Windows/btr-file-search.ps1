e24f5639dfea990e501486af5f69bf0f454f650b
meyyh <94275329+meyyh@users.noreply.github.com>
2023-11-29 15:36:37 -0600

    Update btr-file-search.ps1
    nailed it

--- scripts/Windows/btr-file-search.ps1
+++ scripts/Windows/btr-file-search.ps1
@@ -1,2 +1,5 @@
-cur_users = Get-LocalUser | Select-Object Name | ForEach-Object { $_.Name }
-gci -r -include *.jpg,*.png,*.aac,*.ac3,*.avi,*.aiff,*.bat,*.bmp,*.exe,*.flac,*.gif,*.jpeg,*.mov,*.m3u,*.m4p,*.mp2,*.mp3,*.mp4,*.mpeg4,*.midi,*.msi,*.ogg,*.png,*.txt,*.sh,*.wav,*.wma,*.vqf,*.pcap,*.zip,*.pdf,*.json 'C:\Users\${cur_users}'
+$localusers = Get-LocalUser | Where-Object -Property Enabled -eq True | Select-Object -ExpandProperty Name
+
+foreach ($user in $localusers) {
+ls -r -include *.jpg,*.png,*.aac,*.ac3,*.avi,*.aiff,*.bat,*.bmp,*.exe,*.flac,*.gif,*.jpeg,*.mov,*.m3u,*.m4p,*.mp2,*.mp3,*.mp4,*.mpeg4,*.midi,*.msi,*.ogg,*.png,*.txt,*.sh,*.wav,*.wma,*.vqf,*.pcap,*.zip,*.pdf,*.json "C:\Users\${user}"
+}
