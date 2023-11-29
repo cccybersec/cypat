97b4f06bb92c48c0d286bb43e79291d72ae88060
meyyh <94275329+meyyh@users.noreply.github.com>
2023-11-29 14:47:54 -0600

    Update btr-file-search.ps1

--- scripts/Windows/btr-file-search.ps1
+++ scripts/Windows/btr-file-search.ps1
@@ -1,2 +1,2 @@
-cur_users = Get-LocalUser | Select-Object Name | ForEach-Object { $_.Name } | Select-Object -Skip 1
+cur_users = Get-LocalUser | Select-Object Name | ForEach-Object { $_.Name }
 gci -r -include *.jpg,*.png,*.aac,*.ac3,*.avi,*.aiff,*.bat,*.bmp,*.exe,*.flac,*.gif,*.jpeg,*.mov,*.m3u,*.m4p,*.mp2,*.mp3,*.mp4,*.mpeg4,*.midi,*.msi,*.ogg,*.png,*.txt,*.sh,*.wav,*.wma,*.vqf,*.pcap,*.zip,*.pdf,*.json 'C:\Users\${cur_users}'
