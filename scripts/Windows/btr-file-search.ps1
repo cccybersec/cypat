47749bdf7098a341f9bcf7ebe1b47a6ccddb54d9
meyyh <94275329+meyyh@users.noreply.github.com>
2023-11-29 15:49:15 -0600

    let me know if this has any issues

--- scripts/Windows/btr-file-search.ps1
+++ scripts/Windows/btr-file-search.ps1
@@ -3,3 +3,12 @@ $localusers = Get-LocalUser | Where-Object -Property Enabled -eq True | Select-O
 foreach ($user in $localusers) {
 ls -r -include *.jpg,*.png,*.aac,*.ac3,*.avi,*.aiff,*.bat,*.bmp,*.exe,*.flac,*.gif,*.jpeg,*.mov,*.m3u,*.m4p,*.mp2,*.mp3,*.mp4,*.mpeg4,*.midi,*.msi,*.ogg,*.png,*.txt,*.sh,*.wav,*.wma,*.vqf,*.pcap,*.zip,*.pdf,*.json "C:\Users\${user}"
 }
+
+
+$response = (Read-Host "delete all files listed above (y/n)").ToLower()
+if ($response -eq 'y') {
+        Remove-Item -recurse -Force -Include *.jpg,*.png,*.aac,*.ac3,*.avi,*.aiff,*.bat,*.bmp,*.exe,*.flac,*.gif,*.jpeg,*.mov,*.m3u,*.m4p,*.mp2,*.mp3,*.mp4,*.mpeg4,*.midi,*.msi,*.ogg,*.png,*.txt,*.sh,*.wav,*.wma,*.vqf,*.pcap,*.zip,*.pdf,*.json "C:\Users\${user}"
+}
+else {
+        Write-Host "then do it your self"
+}
