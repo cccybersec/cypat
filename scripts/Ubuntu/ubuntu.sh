1b7cf5fb85e75476a1c9af6135ba3fed733d62bf
meyyh <94275329+meyyh@users.noreply.github.com>
2023-12-11 10:46:45 -0600

    Update ubuntu.sh

--- scripts/Ubuntu/ubuntu.sh
+++ scripts/Ubuntu/ubuntu.sh
@@ -12,7 +12,7 @@ fi
 #VARS!V!V!V!V!V!V!V!
 #yolo=false echo things yolo=true do the things like remove users and edit files
 YOLO=false
-# the name of the debain version like bullseye or bookworm
+# the name of the ubuntu version like bullseye or snapbloat
 ubuver=focal
 # get users & admins from the README you can use chat gpt for easy formatting ("user" "user" "user")
 users=("pverisof" "rolivaw" "jfara" "lpirenne" "lponyets" "lcrast" "shardin" "tsutt" "yfulham" "ylee" "dvenabili" "gtrevize" "jpelorat")
@@ -21,13 +21,30 @@ admins=("gdornick" "hseldon" "hmallow" "adarell")
 
 _backup-files(){
 ## make copys of EVERYTHING that is changed in case my code hits the fan
-buppaths=("/etc/pam.d/common-auth" "/etc/pam.d/common-password" "/etc/shadow" "/etc/group" "/etc/passwd" "/etc/ssh/sshd_config" "/etc/apt/sources.list")
+buppaths=("/etc/pam.d/common-auth" "/etc/pam.d/common-password" "/etc/login.defs" "/etc/shadow" "/etc/group" "/etc/passwd" "/etc/ssh/sshd_config" "/etc/apt/sources.list" "/etc/adduser.conf" "/etc/audit/auditd.conf" "/etc/audit/rules.d/hardening.rules" "/etc/pam.d/common-password" "/etc/pam.d/common-account" "/etc/pam.d/common-auth" "/etc/systemd/coredump.conf" "/etc/default/grub.d" "/etc/modprobe.d/disablefs.conf" "/etc/modprobe.d/disablemod.conf" "/etc/modprobe.d/disablenet.conf" "/etc/security/faillock.conf" "/etc/systemd/journald.conf" "/etc/security/limits.conf" "/etc/systemd/logind.conf" "/etc/login.defs" "/etc/logrotate.conf" "/etc/pam.d/login" "/etc/psad/psad.conf" "/etc/psad/auto_dl" "/etc/systemd/resolved.conf" "/etc/default/rkhunter" "/etc/rsyslog.conf" "/etc/security/access.conf" "/etc/ssh/ssh_config" "/etc/ssh/sshd_config" "/etc/sysctl.conf" "/etc/systemd/system.conf" "/etc/systemd/timesyncd.conf" "/etc/default/ufw" "/etc/default/useradd" "/etc/systemd/user.conf"
+)
 
 # funky sed sheniganders that appends .bak to these ^
-for buppath in $buppaths
+for buppath in ${buppaths[@]}
 do	
 	    sed '' $buppath -i.bak
 done
+}
+
+_config_diff(){
+dpkg-query -W -f='${Conffiles}\n' '*' | awk 'OFS="  "{print $2,$1}' | LANG=C md5sum -c 2>/dev/null | awk -F': ' '$2 !~ /OK$/{print $1}'
+
+echo "if anything shows up press no and check the file/s then rerun script and press yes"
+
+# Ask the user if they want to continue
+read -p "Do you want to continue? (y/n): " exit_script
+
+if [[ "$exit_script" == "y" || "$exit_script" == "Y" ]]; then
+    echo "onwards"
+else
+    echo "so long pardner"
+    exit 0
+fi
 }
 
 
@@ -45,15 +62,16 @@ fi
 }
 
 
-_pkgsearch(){
+_pkgsearch(){ ## add note about libpam-cracklib in run section to echo to user
 #search for no no haxor man packages
-badpkgs=("nmap" "zenmap" "apache2" "nginx" "lighttpd" "wireshark" "tcpdump" "netcat" "nikto" "crack" "etter" "deluge" "sploit" "john" "ydra" "burp" "strike" "veil" "darkcomet" "empire")
+badpkgs=("nmap" "zenmap" "apache" "nginx" "lighttpd" "wireshark" "tcpdump" "netcat" "nikto" "crack" "etter" "deluge" "sploit" "john" "ydra" "burp" "strike" "veil" "darkcomet" "empire")
 
 for badpkg in "${badpkgs[@]}"; do
 	dpkg-query --showformat='${Package}\n' --show | grep $badpkg
 	if [ $? -eq 0 ]; then
 		if [ YOLO == true ]; then
-			echo ${Package} | grep "\S" | apt remove #!#!# need to test if this works
+			#echo ${Package} | grep "\S" | apt remove #!#!# need to test if this works
+			echo ${Package} | grep "\S"
 		else
 			#the "\S" removes blank lines
 			echo ${Package} | grep "\S"
@@ -85,19 +103,11 @@ done
 }
 
 
-_ufw(){
-if [ YOLO == true ]; then
-
-else
-
-fi
-}
-
 
 _pkgsearch
 _sudoers-check
 _backup
-_ufw
 _uid-check
 _apt-source
+_config_diff
 
